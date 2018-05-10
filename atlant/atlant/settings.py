7f06a5a39c81887d85bd0e3044912b9e87fe2e87
a.shatalin <shatalin89@yandex.ru>
2018-05-10 15:34:51 +0800

    initial

--- atlant/atlant/settings.py
+++ atlant/atlant/settings.py
@@ -120,6 +120,9 @@ APPEND_SLASH=False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "./static/"), )
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'uploaded_media')
 MEDIA_URL = '/media/'
