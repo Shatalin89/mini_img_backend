b49c25327fa7de4ac635d43cc5575252a2cf04ca
a.shatalin <shatalin89@yandex.ru>
2018-05-10 18:11:48 +0800

    add rstatic

--- atlant/atlant/ajax_back/models.py
+++ atlant/atlant/ajax_back/models.py
@@ -1,5 +1,5 @@
 from django.db import models as m
-
+from django.utils.timezone import now
 # Create your models here.
 
 
@@ -7,4 +7,5 @@ class Info(m.Model):
     name = m.CharField(max_length=100, null=True, blank=True)
     description = m.CharField(max_length=3000, null=True, blank=True)
     image = m.ImageField(blank=True, verbose_name=u'Картинка', upload_to='media')
+    datetime_add = m.DateTimeField(default=now, null=True)
 
