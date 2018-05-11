887ff1809c3f921588b4b06ef399e36260099cc9
a.shatalin <shatalin89@yandex.ru>
2018-05-11 15:46:19 +0800

    disbale csrf

--- atlant/atlant/ajax_back/models.py
+++ atlant/atlant/ajax_back/models.py
@@ -6,7 +6,7 @@ from django.utils.timezone import now
 class Info(m.Model):
     name = m.CharField(max_length=100, null=True, blank=True)
     email = m.CharField(max_length=1000, null=True, blank=True)
-    description = m.CharField(max_length=3000, null=True, blank=True)
+    description = m.CharField(max_length=30000, null=True, blank=True)
     image = m.ImageField(blank=True, verbose_name=u'Картинка', upload_to='media')
     datetime_add = m.DateTimeField(default=now, null=True)
 
