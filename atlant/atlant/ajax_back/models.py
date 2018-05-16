40f79518c58e01b1fbb27535e13046f71756d31e
a.shatalin <shatalin89@yandex.ru>
2018-05-16 13:09:04 +0800

    add base64_field

--- atlant/atlant/ajax_back/models.py
+++ atlant/atlant/ajax_back/models.py
@@ -1,6 +1,10 @@
+
 from django.db import models as m
 from django.utils.timezone import now
 # Create your models here.
+
+
+
 
 
 class Info(m.Model):
@@ -8,6 +12,7 @@ class Info(m.Model):
     email = m.CharField(max_length=1000, null=True, blank=True)
     description = m.CharField(max_length=30000, null=True, blank=True)
     image = m.ImageField(blank=True, verbose_name=u'Картинка', upload_to='media')
+    image_base64 = m.ImageField(blank=True, verbose_name=u'Картинка', upload_to='media')
     datetime_add = m.DateTimeField(default=now, null=True)
     winner = m.BooleanField(default=False)
 
